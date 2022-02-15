5a6027846f00aa2e04cac765abf82c3113971939
JHP <codebot053@gmail.com>
2022-02-15 13:19:01 +0900

    initial commit

---
+++ dj_expert/urls.py
@@ -0,0 +1,32 @@
+"""dj_expert URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from ninja import NinjaAPI
+
+api = NinjaAPI()
+
+# 일반 django 와 다르게 view function이
+# 분리되어 있지지않고 urls.py 에서 decorator형태로
+# 같이 볼수 있기 때문에 가독성이 더 좋다.
+@api.get("/add")
+def add(request, a: int, b: int):
+    return {"result":a+b}
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", api.urls),
+]
